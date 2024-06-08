c48613237f9c1d6c8dcd9f1cbeb9d4859bb9ba4b
Nathan DAUNOIS <nathandaunois@sfr.fr>
2024-06-08 15:41:37 +0200

    Day1 second parrt
    Signed-off-by: Nathan DAUNOIS <nathandaunois@sfr.fr>

--- Day1/day1.rb
+++ Day1/day1.rb
@@ -1,27 +1,46 @@
+# First Day of coding challenge in ruby!
 module Program
+  SLIDING_WINDOW_SIZE = 3
 
   module_function
 
   def read_from_file
     array = []
-    File.foreach("input.txt") { |line| array << line.to_i }
-    return array
+    File.foreach('input.txt') { |line| array << line.to_i }
+    array
   end
 
-  def day1(array)
+  def day1_first_part(array)
     last_element = -1
     counter = 0
     array.each do |element|
-      unless last_element == -1
-        if last_element < element
-          counter = counter + 1
-        end
-      end
+      counter += 1 if last_element != -1 && last_element < element
       last_element = element
     end
-    return counter
+    counter
+  end
+  def compute_array_sums(array)
+    window_indexes = [-2, -1, 0]
+    windows_dict = {}
+    windows_dict.default_proc = proc { |dict, key| dict[key] = [] }
+    array.each_with_index do |element, index|
+      window_indexes.each do |window_index|
+        unless window_index < 0
+          windows_dict[window_index] << element
+        end
+      end
+      window_indexes = window_indexes.map { |index| index + 1 }
+    end
+    windows_dict.map do |_, value|
+      value.sum
+    end
+  end
+  def day1_second_part(array)
+    array = compute_array_sums(array)
+    day1_first_part(array)
   end
 end
 
-array = Program::read_from_file
-puts Program::day1(array)
+array = Program.read_from_file
+puts Program.day1_first_part(array)
+puts Program.day1_second_part(array)
