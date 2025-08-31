9668481eacae1a27d2fd0e78d3fe8ece01f70583
ultimate318ai <nathandaunois@sfr.fr>
2025-08-31 10:32:13 +0200

    add grid operations

--- Day4/day4.rb
+++ Day4/day4.rb
@@ -6,27 +6,47 @@ module Day4
       @height = height
       @width = width
       @grid_values = values.each_with_index.with_object({}) do |(element, index), grid|
-        puts grid
-        puts element
-        puts index
         value_width_position = index % height
         value_height_position = index / width
         grid[value_height_position] = {} unless grid.key?(value_height_position)
-        grid[value_height_position][value_width_position] = element
+        grid[value_height_position][value_width_position] = { value: element, marked?: false }
       end
+    end
+
+    def mark_all(value)
+      @grid_values.each do |row|
+        row.each do |column_element|
+          column_element[:marked?] = true if column_element[:value] == value
+        end
+      end
+    end
+
+    def grid_winning?
+      row_winning? || column_winning?
     end
 
     def to_s
       @grid_values
     end
+
+    private
+
+    def row_winning?
+      @grid_values.any? do |row|
+        row.all? do |column_element|
+          column_element[:marked?] = true
+        end
+      end
+    end
+
+    def column_winning?
+      @grid_values.all? do |row|
+        row.first[:marked?]
+      end
+    end
   end
 end
 
 # input_array = Day4.read_from_file
-puts Day4::Grid.new(5, 5, [22, 13, 17, 11, 0,
-                           8, 2, 23, 4, 24,
-                           21, 9, 14, 16, 7,
-                           6, 10, 3, 18,  5,
-                           1, 12, 20, 15, 19])
 # puts Day4.first_part(input_array)
 # puts Day4.second_part(input_array)
